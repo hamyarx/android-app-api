67166e1186f67a6d1e22d0683bc953b7d8177380
ashkanfekri <ashkanfekridev@gmail.com>
2023-12-23 01:33:15 +0330

    Removed all files that are in the .gitignore

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$config = require_once __DIR__ . '/../app/config.php';
+
+echo $config['app']['name'];
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd98cdb8f20ed2407d569b4cc8b8ad73623b8bc59',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd98cdb8f20ed2407d569b4cc8b8ad73623b8bc59',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
