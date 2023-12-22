b97e5e8977f65b89a3471df65b9af9c94e269bc0
ashkanfekri <ashkanfekridev@gmail.com>
2023-12-23 01:27:09 +0330

    add base configs

---
+++ app/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    // base application configs
+    "app" => [
+        "name" => 'android-app-api',
+        "url" => 'localhost',
+        "port" => '8000',
+        "php_version" => PHP_VERSION,
+        "version" => "0.0.0",
+    ],
+    // maintenance mode
+    'maintenance_mode' => false,
+    // database configs
+    "database" => [
+        'user_name' => 'root',
+        "password" => '',
+        'host' => 'localhost',
+    ]
+];
+
